0572ac0b2eca176c83cf8bcf8c3ae5f30e32541a
luanpjesus <luanpjesus@gmail.com>
2026-08-19 12:42:07 -0300

    db login created/fix the errors

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # from dbm import sqlite3
 
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_login import LoginManager
 
 from database import db
 from models.user import User
@@ -10,10 +11,30 @@ app.config["SECRET_KEY"] = "your_secret_key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
 
+login_manager = LoginManager()
+
 # Session <- conexao ativa - abrir e fechar a sessao
 # Banco relacional - Banco que armazena informacoes em tabelas e elas se relacionam entre si
 
+
+# Iniciao junto com o App
 db.init_app(app)
+login_manager.init_app(app)
+
+
+# view login
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        pass
+
+    return jsonify({"message": "credenciais invalidas"}), 400
 
 
 @app.route("/hello-world", methods=["GET"])
